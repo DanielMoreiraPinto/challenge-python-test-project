7786717a743acdd880dec003175ffbdb74b86565
Daniel Moreira Pinto <danielmoreirapinto.ti@gmail.com>
2022-02-19 18:07:23 -0300

    Coleta dos dados implementada

--- src/booking_controller.py
+++ src/booking_controller.py
@@ -1,6 +1,8 @@
 from os import getcwd, listdir, path
 from re import sub
 from datetime import datetime
+from csv import DictReader
+from tkinter.tix import InputOnly
 
 from src.hotel import Hotel
 
@@ -13,7 +15,8 @@ class BookingController():
         
     def assist_booking(self):
         self.__get_hotels()
-        input_str = 'Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)'
+
+        input_str = input()
         client_type, dates = self.__process_input(input_str)
         
         qtd_weekdays = qtd_weekends = 0
@@ -41,7 +44,7 @@ class BookingController():
         
     def __process_input(self, input_str):
         client_type, dates_unformatted = input_str.replace(' ', '').split(':')
-        dates_unformatted = sub(r"\([^()]*\)", "", dates_unformatted)
+        dates_unformatted = sub(r'\([^()]*\)', '', dates_unformatted)
         dates = []
         for date_unformatted in dates_unformatted.split(','):
             date = datetime.strptime(date_unformatted, '%d%b%Y')
@@ -49,10 +52,16 @@ class BookingController():
         return (client_type, dates)
     
     def __get_hotels(self):
-        lakewood = Hotel('Lakewood', 3, 110, 90, 80, 80)
-        bridgewood = Hotel('Bridgewood', 4, 160, 60, 110, 50)
-        ridgewood = Hotel('Ridgewood', 5, 220, 150, 100, 40)
-        hotels = [lakewood, bridgewood, ridgewood]
+        hotels = []
+        with open(path.join(self.__data_dir, 'hotel_data.csv'), 'r') as data_file:
+            csv_dict_reader = DictReader(data_file)
+            for row in csv_dict_reader:
+                hotel = Hotel(row['name'], row['rating'], 
+                              row['weekday_regular'],
+                              row['weekend_regular'], 
+                              row['weekday_reward'],
+                              row['weekend_reward'])
+                hotels.append(hotel)
         self.__hotels = hotels
         
          
